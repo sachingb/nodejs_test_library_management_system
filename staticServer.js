4547017e72cc78c116bbfc601ddaee4b65970604
sachin <sachin@redmonstergames.com>
2016-06-25 13:09:43 +0530

    Adding comments

--- staticServer.js
+++ staticServer.js
@@ -1,6 +1,7 @@
 var connect = require("connect");
 var serveStatic = require("serve-static");
 
+//to server index.html
 function start(){
    connect().use(serveStatic("webpage")).listen(8080, function(){
       console.log("Http server runninf on 8080");
